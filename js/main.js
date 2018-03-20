2806d91d4eed375c4cba135f7f84211802bbd236
smontoro <sarahjmontoro@gmail.com>
2018-03-19 22:41:25 -0700

    hover, on, this

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 //when i click on the h1, i want it to change styles
-$("h1").click(function() {
+$("h1").hover(function() {
 	//$("body").toggleClass("dark new-style")
 	//$("h1").toggleClass("new-style")
 
-	$("h1").css(
+	$(this).css(
 		{
 			"background-color": "purple", 
 			"color": "white",
 			"text-transform": "lowercase"
 		}
 	)
+},
+
+	function(){
+		$(this).css(
+		{
+			"background-color": "", 
+			"color": "",
+			"text-transform": ""
+		}
+	)
+
+
 })
+
