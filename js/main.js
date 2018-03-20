27469bdd8580bcb5f39fcb564f9e86440c4b7a8a
smontoro <sarahjmontoro@gmail.com>
2018-03-19 17:19:38 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*$(document).ready(
+	function() {
+	//do stuff
+	}
+);*/
+
+//when i click the button, 
+/*$("button").click(
+
+
+//i wont the background color of body to change
+	function() {
+		//document.body.style = "background-color: tomato";
+			$("body").css("background-color", "tomato");
+	}
+)*/
+
+//when i click the button i want the bakground color of the button to change. 
+$("button").click(function(){
+	$("body").css("background-color", "tomato");
+})
