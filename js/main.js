5e8281a92b7d71d55229e9a63f4c3f6bf6b61624
smontoro <sarahjmontoro@gmail.com>
2018-03-19 22:23:19 -0700

    .css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 //when i click on the h1, i want it to change styles
 $("h1").click(function() {
-	$("body").toggleClass("dark new-style")
+	//$("body").toggleClass("dark new-style")
 	//$("h1").toggleClass("new-style")
+
+	$("h1").css(
+		{
+			"background-color": "purple", 
+			"color": "white",
+			"text-transform": "lowercase"
+		}
+	)
 })
