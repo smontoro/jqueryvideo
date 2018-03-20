e72ea662682adc1437d3a3a202c6a27484c86f8d
smontoro <sarahjmontoro@gmail.com>
2018-03-19 18:06:38 -0700

    toggle

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //element/type selector
-document.getElementsByTagName("button")
+document.getElementsByTagName("button") //vanilla JS
 $("button")
 
 //class selector
@@ -12,3 +12,24 @@ var cta = $("#cta")
 
 //this selector
 $(this)
+
+//when I click o any element with class "box"; I wan to do stuff
+$(".box").click(function() {
+	//does stuff
+})
+
+// .show()
+// .hide()
+//when I click the button, i want the image to hide
+$("#hide").click(function() {
+	$("img").hide();
+})
+//when i click the show button, i want the image to show
+$("#show").click(function() {
+	$("img").show();
+})
+
+//when i click toggle, i want the image to hide OR show
+$("#toggle").click(function() {
+	$("img").toggle()
+})
